ee3a9ee42c74e5602d3a80b9f6465ec275ff28d6
Aaditya Benke <aadityabenke@gmail.com>
2025-02-20 18:09:12 +0530

    pls bro

--- example/src/index.js
+++ example/src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import HomePage from './landing_page/home/HomePage';
+import SignUp from './landing_page/signup/Signup';
+import AboutPage from './landing_page/about/AboutPage';
+import ProductPage from './landing_page/products/ProductsPage';
+import PricingPage from './landing_page/pricing/PricingPage';
+import SupportPage from './landing_page/support/SupportPage';
+import Navbar from './landing_page/Navbar';
+import Footer from './landing_page/Footer';
+import NotFound from './landing_page/NotFound';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+  <Navbar></Navbar>
+    <Routes>
+      <Route path="/" element={<HomePage />}></Route>
+      <Route path="/signup" element={<SignUp />}></Route>
+      <Route path="/about" element={<AboutPage />}></Route>
+      <Route path="/product" element={<ProductPage />}></Route>
+      <Route path="/pricing" element={<PricingPage />}></Route>
+      <Route path="/support" element={<SupportPage />}></Route>
+      <Route path="*" element={<NotFound />}></Route>
+    </Routes>
+    <Footer></Footer>
+  </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
